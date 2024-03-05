e5215de65367f8c3d6088998cace0e89c8388d09
GustavoFoss <fossgustavo8@gmail.com>
2024-03-04 23:05:19 -0300

    formulario concluido

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,15 +1,32 @@
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 import './Formulario.css'
 
 const Formulario = () => {
 
+    const times = [
+        'Adc',
+        'Tank',
+        'Mid',
+        'Jungle',
+        'Support'
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log("Form submetido!")
+    }
+
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
-                <CampoTexto label="Nome" placeholder="Digite o seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
-                <CampoTexto label="Imagem" placeholder="Digite o endereço de imagem"/>
+                <CampoTexto obrigatorio= {true} label="Nome" placeholder="Digite o seu nome" />
+                <CampoTexto obrigatorio= {true} label="Cargo" placeholder="Digite o seu cargo"/>
+                <CampoTexto obrigatorio= {true} label="Imagem" placeholder="Digite o endereço de imagem"/>
+                <ListaSuspensa obrigatorio= {true} label="Selecione o time" itens= {times} />
+                <Botao>Criar Card</Botao>
             </form>
         </section>
     );
