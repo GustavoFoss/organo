33ce7d8b3c2e5c4b2a0dc48cdfd56c2a7d9e5cde
GustavoFossSilva <gustavosilva.nextaage@gmail.com>
2024-03-05 15:42:37 -0300

    times sections

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
 
@@ -15,6 +16,11 @@ function App() {
     <div className="App">
       <Banner/>
       <Formulario aoColaboradorCadastrado = {colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Time nome={"ADC"}/>
+      <Time nome={"TANK"}/>
+      <Time nome={"MID"}/>
+      <Time nome={"JUNGLE"}/>
+      <Time nome={"SUPPORT"}/>
     </div>
   );
 }
